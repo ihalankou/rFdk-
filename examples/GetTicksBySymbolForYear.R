2f12eb7791fb0a24e4522f7b3a21bf041dc815e3
ihalankou <ihar.halankou@soft-fx.lv>
2016-08-12 11:13:27 +0300

    Fix script

--- examples/GetTicksBySymbolForYear.R
+++ examples/GetTicksBySymbolForYear.R
@@ -2,7 +2,7 @@ if(!require("lubridate")) {
 install.packages("lubridate", dependencies = TRUE,  repos = "http://cran.us.r-project.org") 
 library(lubridate) 
 }
-symbol <- "EURUSD" 
+symbols <- "EURUSD" 
 # get ticks by EUR/USD for 1 year (from 2012-01-01 to 2013-01-01) 
 startTime=ymd(20120101) 
 endTime= startTime + years(1) 
