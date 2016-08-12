9b40ba5b0d61c2bedcdc1e06392c47d1716b0ec4
ihalankou <ihar.halankou@soft-fx.lv>
2016-08-12 11:09:56 +0300

    Modify symbol

--- examples/GetBarsBySymbolForYear.R
+++ examples/GetBarsBySymbolForYear.R
@@ -2,7 +2,7 @@ if(!require("lubridate")) {
 install.packages("lubridate", dependencies = TRUE,  repos = "http://cran.us.r-project.org")  
 library(lubridate)  
 } 
-symbol <- "EUR/USD"
+symbol <- "EURUSD"
 # get bars M1 by EUR/USD for 1 year (from 2012-01-01 to 2013-01-01)
 startTime=ymd(20120101)
 endTime= startTime + years(1)
